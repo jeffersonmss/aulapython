7bedc3d21c4a83143053bdb69dc0a46effae7da9
Jefferson Soares da Silva <jefferson.mss@gmail.com>
2024-05-20 19:02:09 -0300

    Alteração na aula de string

---
+++ lista.py
@@ -0,0 +1,10 @@
+'''
+Adição (+): concatena duas listas.
+Multiplicação (*): repete a lista um determinado número de vezes.
+- Fatiamento ([inicio:fim:passo]) : extrai um subconjunto da lista.
+- len(lista): retorna o tamanho da lista (número de elementos).
+- append(elemento): adiciona um elemento ao final da lista.
+- insert(indice, elemento): insere um elemento em um determinado índice da lista.
+- remove(elemento): remove a primeira ocorrência do elemento da lista.
+- pop(indice): remove o elemento de um determinado índice da lista e retorna o elemento removido.
+'''
