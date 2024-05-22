454214573696beacc5ecc6ddf845e30752612a0d
Jefferson Soares da Silva <jefferson.mss@gmail.com>
2024-05-22 19:10:13 -0300

    Adicionando novas mudanças

--- string.py
+++ string.py
@@ -1,7 +1,32 @@
+'''
+upper(): converte a string para maiúsculas.
+lower(): converte a string para minúsculas.
+strip(): remove espaços em branco do início e do final da string.
+replace( antiga, nova ): substitui todas as ocorrências de antiga 
+                         por nova na string.
+split( separador ): divide a string em uma lista de substrings, 
+                    utilizando o separador como delimitador.
+find( substring ): retorna o índice da primeira ocorrência de 
+                   substring na string (ou -1 se não encontrada).
+'''
+
 saudacao = "Bom dia"
 nome = "Jefferson"
 
-print(saudacao + ", " + nome)
+frase = "Tudo me é permitido, mas nem tudo me convém!"
 
-print(nome.upper()) # Escreve tudo em maiusculo
-print(nome.lower()) # Escreve tudo em minusculo
+print(frase.find("m"))
+
+# frase = frase.replace("mas", "porém")
+# print(len(frase))
+
+print(frase[0:len(frase)])
+
+
+# print(saudacao + ", " + nome)
+
+# print(nome.upper()) # Escreve tudo em maiusculo
+# print(nome.lower()) # Escreve tudo em minusculo
+
+
+ # type: ignore
